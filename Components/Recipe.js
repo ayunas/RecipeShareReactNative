33619e8a0a5e850d7ecc73af2ac9594d743f1235
ayunas <ayunas@gmail.com>
2019-10-23 15:54:56 -0500

    set up <RecipeList/> and <Recipe/> renderingusing react-native-elements components

---
+++ Components/Recipe.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {Card, Button, Image, ThemeProvider} from 'react-native-elements';
+
+
+
+const Recipe = props => {
+    
+    return (
+        <View style={{width: 200, alignItems: 'center'}}>
+            <Card title="Recipe Card">
+                <Image 
+                    source={{uri : "https://fakeimg.pl/200x200/?text=sample recipe"}}
+                    style={{width: 200, height: 200 }}
+                />
+                <Button title="Details"/>
+            </Card>
+        </View>
+    )
+}
+
+export default Recipe;
---
+++ Components/RecipeList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import samples from '../assets/samplerecipes.json'
+import {ScrollView, Text, View} from 'react-native';
+import Recipe from './Recipe';
+
+
+const RecipeList = () => {
+    console.log('sample recipes json', samples);
+
+    return (
+        <ScrollView >
+            <View style={{alignItems: 'center'}}>
+            {samples.map( (recipe,i) => <Recipe key={i} recipe={recipe} />)}
+            </View>
+        </ScrollView>
+    )
+}
+
+export default RecipeList;
+
+
